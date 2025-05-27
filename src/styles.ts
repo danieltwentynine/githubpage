9387c9e9333b26e5a7e25f50f8e78aa515594dfe
danieltwentynine <danielmcardoso2016@protonmail.com>
2025-05-27 20:22:27 -0300

    aprendendo styled-components

---
+++ src/styles.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+    margin: 0;
+    padding: 0;
+    border-box: box-sizing;
+`
+export default EstiloGlobal
